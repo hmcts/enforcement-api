4191a0391f8be338ebb9d44005575edaa4634dfa
Peter Pilgrim <peter.pilgrim@gmail.com>
2025-03-19 15:25:56 +0000

    HDPI-182 Apply Scott's feedback - removed Hamcrest dependency

--- src/test/java/uk/gov/hmcts/reform/enforcement/demo/DemoUnitTest.java
+++ src/test/java/uk/gov/hmcts/reform/enforcement/demo/DemoUnitTest.java
@@ -3,8 +3,6 @@ package uk.gov.hmcts.reform.enforcement.demo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DemoUnitTest {
@@ -12,12 +10,5 @@ class DemoUnitTest {
     @Test
     void exampleOfTest() {
         assertTrue(System.currentTimeMillis() > 0,"Example of Unit Test");
-    }
-
-    @DisplayName("should demonstrate to write a test with hamcrest")
-    @Test
-    void shouldDemonstrateHamcrest() {
-        assertThat("Hello World", is("Hello World"));
-        assertThat(1 + 2 + 3 + 4 + 5, is(15));
     }
 }
--- src/test/java/uk/gov/hmcts/reform/enforcement/demo/service/EnforcementServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/enforcement/demo/service/EnforcementServiceTest.java
@@ -1,25 +1,28 @@
 package uk.gov.hmcts.reform.enforcement.demo.service;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.reform.enforcement.demo.model.AccountHolder;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class EnforcementServiceTest {
+    @DisplayName("should calculate fine")
     @Test
     void calculateFine() {
         var enforcementService = new EnforcementService();
-        assertThat(enforcementService.calculateFine(100,70),is(300.0));
+        assertThat(enforcementService.calculateFine(100,70)).isEqualTo(300.0);
     }
 
+    @DisplayName("should settle fine for account holder")
     @Test
     void settleFineForAccountHolder() {
         var accountHolder = AccountHolder.builder()
             .accountName("Jim Bean").accountNumber("54321").balance(1000).penalty(0).build();
         var enforcementService = new EnforcementService();
         var accountHolderWithFine = enforcementService.settleFineForAccountHolder(accountHolder,100,70);
-        assertThat(accountHolderWithFine,is(
-            AccountHolder.builder().accountName("Jim Bean").accountNumber("54321").balance(700).penalty(300).build()));
+        assertThat(accountHolderWithFine).isEqualTo(
+            AccountHolder.builder().accountName("Jim Bean").accountNumber("54321").balance(700).penalty(300).build());
     }
 }
