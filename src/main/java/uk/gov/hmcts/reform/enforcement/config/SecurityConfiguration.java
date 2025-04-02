fe22de48686da6dc947fcebf7c2bd1fd6a309686
Cebrail Ergisi <cebrail.ergisi@gmail.com>
2025-04-02 14:59:36 +0100

    Revert "Remove super. Replace assertions with AssertJ. Remove duplicate exclusions"
    This reverts commit 55147d5d8359b151b2201c6faaa17ce4b05289a0.

--- src/main/java/uk/gov/hmcts/reform/enforcement/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/enforcement/config/SecurityConfiguration.java
@@ -28,6 +28,7 @@ public class SecurityConfiguration {
 
     @Autowired
     public SecurityConfiguration(ServiceAuthFilter serviceAuthFilter) {
+        super();
         this.serviceAuthFilter = serviceAuthFilter;
     }
 
@@ -45,11 +46,11 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.addFilterBefore(serviceAuthFilter, AbstractPreAuthenticatedProcessingFilter.class)
-            .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(STATELESS))
-            .httpBasic(AbstractHttpConfigurer::disable)
-            .formLogin(AbstractHttpConfigurer::disable)
-            .logout(AbstractHttpConfigurer::disable)
-            .csrf(AbstractHttpConfigurer::disable);
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(STATELESS))
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
