3c524a8fa27ca2afd864fa2ad371d773d571903b
Cebrail Ergisi <cebrail.ergisi@gmail.com>
2025-04-02 15:55:23 +0100

    implemented notifications for enforcement api

---
+++ src/test/java/uk/gov/hmcts/reform/enforcement/notify/service/NotificationServiceTest.java
@@ -0,0 +1,86 @@
+package uk.gov.hmcts.reform.enforcement.notify.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import uk.gov.hmcts.reform.enforcement.notify.exception.NotificationException;
+import uk.gov.hmcts.reform.enforcement.notify.model.EmailNotificationRequest;
+import uk.gov.service.notify.NotificationClient;
+import uk.gov.service.notify.NotificationClientException;
+import uk.gov.service.notify.SendEmailResponse;
+
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class NotificationServiceTest {
+
+    @Mock
+    private NotificationClient notificationClient;
+
+    @InjectMocks
+    private NotificationService notificationService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testSendEmailSuccess() throws NotificationClientException {
+        var emailRequest = new EmailNotificationRequest(
+                "test@example.com",
+                "templateId",
+                new HashMap<>(),
+                "reference",
+                "emailReplyToId"
+        );
+        var sendEmailResponse = mock(SendEmailResponse.class);
+        when(sendEmailResponse.getNotificationId())
+                .thenReturn(UUID.fromString("550e8400-e29b-41d4-a716-446655440000"));
+        when(sendEmailResponse.getReference()).thenReturn(Optional.of("reference"));
+        when(notificationClient.sendEmail(anyString(), anyString(), anyMap(), anyString()))
+                .thenReturn(sendEmailResponse);
+
+        var response = notificationService.sendEmail(emailRequest);
+
+        assertNotNull(response);
+        assertEquals(UUID.fromString("550e8400-e29b-41d4-a716-446655440000"), response.getNotificationId());
+        verify(notificationClient, times(1))
+                .sendEmail(anyString(), anyString(), anyMap(), anyString());
+    }
+
+    @Test
+    void testSendEmailFailure() throws NotificationClientException {
+        var emailRequest = new EmailNotificationRequest(
+                "test@example.com",
+                "templateId",
+                new HashMap<>(),
+                "reference",
+                "emailReplyToId"
+        );
+        when(notificationClient.sendEmail(anyString(), anyString(), anyMap(), anyString()))
+                .thenThrow(new NotificationClientException("Error"));
+
+        var exception = assertThrows(
+                NotificationException.class, () ->
+                        notificationService.sendEmail(emailRequest)
+        );
+
+        assertEquals("Email failed to send, please try again.", exception.getMessage());
+        verify(notificationClient, times(1))
+                .sendEmail(anyString(), anyString(), anyMap(), anyString());
+    }
+}
