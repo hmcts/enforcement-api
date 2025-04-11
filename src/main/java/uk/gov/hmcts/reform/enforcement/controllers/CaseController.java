868fc3c24be7424ec69e2899e4e1b95d44594024
Cebrail Ergisi <cebrail.ergisi@gmail.com>
2025-04-11 10:04:58 +0100

    Remove star imports for SonarQube checks

--- src/main/java/uk/gov/hmcts/reform/enforcement/controllers/CaseController.java
+++ src/main/java/uk/gov/hmcts/reform/enforcement/controllers/CaseController.java
@@ -3,7 +3,10 @@ package uk.gov.hmcts.reform.enforcement.controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.enforcement.model.SectionResponse;
 import uk.gov.hmcts.reform.enforcement.model.Task;
 
