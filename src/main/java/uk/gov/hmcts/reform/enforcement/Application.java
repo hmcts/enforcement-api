c252b8770dc5d13d3353143b383c8cdd12621b3a
trevor@trevorhinson.com <trevor@promolli.com>
2025-05-01 09:32:40 +0100

    WIP push.
    Add initial CCD and XUI integrations with supporting configurations
    
    This commit introduces configuration changes for integrating with CCD and XUI, including Gradle dependencies, CCD roles, enums, states, and Helm chart entries. It also adds support for CCD definitions using CFTLib along with Docker and environment updates to facilitate local development. Additionally, new classes and platform configuration files required for the integration are included.

--- src/main/java/uk/gov/hmcts/reform/enforcement/Application.java
+++ src/main/java/uk/gov/hmcts/reform/enforcement/Application.java
@@ -2,9 +2,20 @@ package uk.gov.hmcts.reform.enforcement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import uk.gov.hmcts.reform.idam.client.IdamApi;
 
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackages = {
+        "uk.gov.hmcts.reform.enforcement",
+        "uk.gov.hmcts.ccd.sdk"
+    })
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
+@EnableFeignClients(
+    clients = {
+        IdamApi.class
+    }
+)
 public class Application {
 
     public static void main(final String[] args) {
