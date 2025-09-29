9bf3eabffc4330e20c87f8427914b6668869fc74
tvr-solirius <trevor.hinson@solirius.com>
2025-09-29 09:09:37 +0100

    TO pass the checkstyle.

--- src/main/java/uk/gov/hmcts/reform/enforcement/controllers/RootController.java
+++ src/main/java/uk/gov/hmcts/reform/enforcement/controllers/RootController.java
@@ -23,7 +23,7 @@ public class RootController {
      */
     @GetMapping("/")
     public ResponseEntity<String> welcome() {
-        return ok("Just testing what is in place." +
-                      "Welcome to the Enforcement API");
+        return ok("Just testing what is in place."
+                      + "Welcome to the Enforcement API");
     }
 }
